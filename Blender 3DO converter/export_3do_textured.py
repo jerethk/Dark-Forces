852d20081b05b61a218fdea4742a0f59a075144d
jerethk <82040941+jerethk@users.noreply.github.com>
2023-05-12 03:52:05 +1000

    Add files via upload

--- Blender 3DO converter/export_3do_textured.py
+++ Blender 3DO converter/export_3do_textured.py
@@ -3,42 +3,42 @@ import bmesh
 
 # CREATE 3DO OBJECTS FROM BLENDER MESH OBJECTS
 objects = []
+textures = []
+first_texture_index = 0
 
 for obj in bpy.data.objects:
     if obj.type != 'MESH':
         continue
-    
-    this_object = dict()
+
     mesh = obj.data
-    this_object['name'] = mesh.name
+    
+    # Materials / textures
+    this_obj_textures = []
+    for mat in mesh.materials:
+        this_obj_textures.append(mat.name)
+        
+    textures.append(this_obj_textures)
     
     # Vertices
-    this_object['vertices'] = []
-    this_object['num_vertices'] = len(mesh.vertices)
+    mesh_vertices = []
     for v in mesh.vertices:
-        this_object['vertices'].append((
+        mesh_vertices.append((
             v.co[0],
             v.co[1],
             v.co[2]
         ))
-    
+
     # Determine if polygons can be quads (all polygons have 4 vertices)
+    mesh_copy = mesh.copy()
     is_quads = True
-    this_object['poly_type'] = 'QUADS'
+    poly_type = 'QUADS'
     for p in mesh.polygons:
         if len(p.vertices) != 4:
             is_quads = False
-            this_object['poly_type'] = 'TRIANGLES'
+            poly_type = 'TRIANGLES'
             break
         
-    # Polygons
-    this_object['polygons'] = []
-    this_object['tex_vertices'] = []
-    this_object['tex_polygons'] = []
-    
-    mesh_copy = mesh.copy()
-    
-    # Triangulate if not quads
+    # Triangulate mesh if not quads
     if not is_quads:
         bm = bmesh.new()
         bm.from_mesh(mesh_copy)
@@ -46,40 +46,55 @@ for obj in bpy.data.objects:
         bm.to_mesh(mesh_copy)
         bm.free()
         
-    this_object['num_polygons'] = len(mesh_copy.polygons)
-    colour_counter = 0        # use this for coloring
-    tvert_counter = 0
-    for p in mesh_copy.polygons:
-        this_poly = dict()
-        this_poly['vertices'] = []
-        for v in p.vertices:
-            this_poly['vertices'].insert(0, v)  # vertex order is reversed in Blender (anti-clockwise)
+    # Now have to iterate through materials (textures) and make one object for the polygons which use each one
+    for mat_index in range(len(mesh.materials)):
+        this_object = dict()
+        this_object['name'] = "{}.{}".format(mesh.name, mat_index)
+        this_object['vertices'] = mesh_vertices
+        this_object['num_vertices'] = len(mesh.vertices)
+        this_object['poly_type'] = poly_type
+        this_object['polygons'] = []
+        this_object['tex_vertices'] = []
+        this_object['tex_polygons'] = []
+        
+        colour_counter = 0        # use this for coloring
+        tvert_counter = 0
+        for p in mesh_copy.polygons:
+            if p.material_index == mat_index:
+                this_poly = dict()
+                this_poly['vertices'] = []
+                for v in p.vertices:
+                    this_poly['vertices'].insert(0, v)  # vertex order is reversed in Blender (anti-clockwise)
+                    
+                this_poly['colour'] = 40 + colour_counter  # greys
+                this_poly['shading'] = "texture"
+                this_object['polygons'].append(this_poly)
+
+                colour_counter += 1
+                if colour_counter == 4:
+                    colour_counter = 0
+
+                if not len(mesh_copy.uv_layers):
+                    continue
+
+                # Texture data for polygon
+                tp_verts = []
+                for l in p.loop_indices:
+                    this_object['tex_vertices'].append((
+                        mesh_copy.uv_layers[0].data[l].uv[0],
+                        mesh_copy.uv_layers[0].data[l].uv[1]
+                    ))
+                    tp_verts.insert(0, tvert_counter)  # reverse order
+                    tvert_counter += 1
+                    
+                this_object['tex_polygons'].append(tp_verts)
             
-        this_poly['colour'] = 40 + 4 * colour_counter  # greys
-        this_poly['shading'] = "texture"
-        this_object['polygons'].append(this_poly)
+        this_object['texture'] = mat_index + first_texture_index
+        this_object['num_polygons'] = len(this_object['polygons'])
+        objects.append(this_object)
 
-        colour_counter += 1
-        if colour_counter == 4:
-            colour_counter = 0
-
-        if not len(mesh_copy.uv_layers):
-            continue
-
-        # Texture data for polygon
-        tp_verts = []
-        for l in p.loop_indices:
-            this_object['tex_vertices'].append((
-                mesh_copy.uv_layers[0].data[l].uv[0],
-                mesh_copy.uv_layers[0].data[l].uv[1]
-            ))
-            tp_verts.insert(0, tvert_counter)  # reverse order
-            tvert_counter += 1
-            
-        this_object['tex_polygons'].append(tp_verts)
-    
     bpy.data.meshes.remove(mesh_copy)
-    objects.append(this_object)
+    first_texture_index += len(this_obj_textures)
 
 
 
@@ -97,32 +112,31 @@ def write_3do(context, filepath):
         
         file.write("3DO 1.2 \n")
         file.write("#\n# Created by Jereth's Blender exporter \n#\n")
-        file.write("3DONAME " + filepath + "\n")
+        file.write("3DONAME " + filepath.split("\\")[-1] + "\n")
         file.write("OBJECTS {} \n".format(num_objects))
         file.write("VERTICES {} \n".format(total_vertices))
         file.write("POLYGONS {} \n".format(total_polys))
         file.write("PALETTE DEFAULT.PAL\n\n")
         file.write("TEXTURES {}\n".format(num_objects))
         
-        # Give 1 texture per object
-        for t in range(num_objects):
-            file.write(" TEXTURE: default.bm \t#{} \n".format(t))
+        # Texture table
+        counter = 0
+        for obj_textures in textures:
+            for t in obj_textures:
+                file.write(" TEXTURE: {}.bm \t#{} \n".format(t, counter))
+                counter += 1
             
         file.write("\n")
         
-        object_counter = 0
         for o in objects:
             file.write("#------------------------------------------------------------------\n")
             file.write("OBJECT \"{}\" \n".format(o['name']))
-            file.write("TEXTURE {} \n\n".format(object_counter))
-            object_counter += 1
+            file.write("TEXTURE {} \n\n".format(o['texture']))
             
             file.write("VERTICES {} \n".format(o['num_vertices']))
             file.write("# \tnum: \t  x \t  y \t  z \n")
-            counter = 0
-            for v in o['vertices']:
-                file.write("\t{}:\t{:.3f}\t{:.3f}\t{:.3f}\n".format(counter, v[0], -v[2], v[1]))
-                counter += 1
+            for (n, v) in enumerate(o['vertices']):
+                file.write("\t{}:\t{:.3f}\t{:.3f}\t{:.3f}\n".format(n, v[0], -v[2], v[1]))
                 
             file.write("\n{} {} \n".format(o['poly_type'], o['num_polygons']))
             if o['poly_type'] == 'TRIANGLES':
@@ -130,28 +144,22 @@ def write_3do(context, filepath):
             else:
                 file.write("# \tnum:\t<a>\t<b>\t<c>\t<d>\t<color>\t<shading> \n")
 
-            counter = 0
-            for p in o['polygons']:
+            for (n, p) in enumerate(o['polygons']):
                 if o['poly_type'] == 'TRIANGLES':
-                    file.write("\t{}:\t{}\t{}\t{}\t{}\t{}\n".format(counter, p['vertices'][0], p['vertices'][1], p['vertices'][2], p['colour'], p['shading']))
+                    file.write("\t{}:\t{}\t{}\t{}\t{}\t{}\n".format(n, p['vertices'][0], p['vertices'][1], p['vertices'][2], p['colour'], p['shading']))
                 else:
-                    file.write("\t{}:\t{}\t{}\t{}\t{}\t{}\t{}\n".format(counter, p['vertices'][0], p['vertices'][1], p['vertices'][2], p['vertices'][3], p['colour'], p['shading']))
-                counter += 1
+                    file.write("\t{}:\t{}\t{}\t{}\t{}\t{}\t{}\n".format(n, p['vertices'][0], p['vertices'][1], p['vertices'][2], p['vertices'][3], p['colour'], p['shading']))
             
             file.write("\nTEXTURE VERTICES {} \n".format(len(o['tex_vertices'])))
-            counter = 0
-            for tv in o['tex_vertices']:
-                file.write("\t{}:\t{:.3f}\t{:.3f}\n".format(counter, tv[0], tv[1]))
-                counter += 1
+            for (n, tv) in enumerate(o['tex_vertices']):
+                file.write("\t{}:\t{:.3f}\t{:.3f}\n".format(n, tv[0], tv[1]))
             
             file.write("\nTEXTURE {} {} \n".format(o['poly_type'], len(o['tex_polygons'])))
-            counter = 0
-            for tp in o['tex_polygons']:
+            for (n, tp) in enumerate(o['tex_polygons']):
                 if o['poly_type'] == "TRIANGLES":
-                    file.write("\t{}:\t{}\t{}\t{}\n".format(counter, tp[0], tp[1], tp[2]))
+                    file.write("\t{}:\t{}\t{}\t{}\n".format(n, tp[0], tp[1], tp[2]))
                 else:
-                    file.write("\t{}:\t{}\t{}\t{}\t{}\n".format(counter, tp[0], tp[1], tp[2], tp[3]))
-                counter += 1
+                    file.write("\t{}:\t{}\t{}\t{}\t{}\n".format(n, tp[0], tp[1], tp[2], tp[3]))
                 
             
             file.write("\n")
